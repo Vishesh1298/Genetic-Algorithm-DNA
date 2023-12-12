9da35803d46d2859d9e4089c80f941ceda056cf7
Vishesh Kapoor <57246624+Vishesh1298@users.noreply.github.com>
2023-12-12 14:00:40 +0000

    modified code

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,7 +1,8 @@
-import java.util.List; // Add this import statement
+import java.util.List;
 
-public class GeneticAlgorithm {
+public class GeneticAlgorithm implements Operator {
     public static void main(String[] args) {
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(); // Create an instance
         int populationSize = 10; // Specify the desired population size
         String chromosomeType = "binary"; // Specify the desired chromosome type (binary or integer)
 
@@ -12,10 +13,73 @@ public class GeneticAlgorithm {
         // Get the chromosomes from the population
         List<Chromosome> chromosomes = population.getChromosomes();
 
-        // Print each binary chromosome
+        // Print each chromosome
         for (int i = 0; i < chromosomes.size(); i++) {
-            BinaryChromosome binaryChromosome = (BinaryChromosome) chromosomes.get(i);
-            System.out.println("Chromosome " + (i + 1) + ": " + binaryChromosome.getGeneticCode());
+            Chromosome chromosome = chromosomes.get(i);
+            System.out.println("Chromosome " + (i + 1) + ": " + chromosome.getGeneticCode());
         }
+
+        // Apply default strategies or set custom ones
+        geneticAlgorithm.applyMutation(population);
+        geneticAlgorithm.applyCrossover(population);
+        geneticAlgorithm.applyMutation(population);
+
+        // Further steps in your genetic algorithm...
+    }
+
+    public void applyCrossover(Population population) {
+        // Implement crossover logic here
+    }
+
+    @Override
+    public SelectionStrategy getDefaultSelectionStrategy() {
+        return new RouletteWheelSelection(); // or any other default strategy
+    }
+
+    @Override
+    public CrossoverStrategy getDefaultCrossoverStrategy() {
+        return new SinglePointCrossover(); // or any other default strategy
+    }
+
+    @Override
+    public MutationStrategy getDefaultMutationStrategy() {
+        return new BitFlipMutation(); // or any other default strategy
+    }
+
+    // Implement other methods from the Operator interface...
+    
+    @Override
+    public void initializePopulation(Population population) {
+        // Implementation for initializing population
+    }
+
+    @Override
+    public void evaluateFitness(Population population) {
+        // Implementation for evaluating fitness
+    }
+
+    @Override
+    public void calculateFitness(Chromosome chromosome) {
+        // Implementation for calculating fitness
+    }
+
+    @Override
+    public void getBestChromosome(Population population) {
+        // Implementation for getting the best chromosome
+    }
+
+    @Override
+    public void setSelectionStrategy(SelectionStrategy strategy) {
+        // Implementation for setting selection strategy
+    }
+
+    @Override
+    public void setCrossoverStrategy(CrossoverStrategy strategy) {
+        // Implementation for setting crossover strategy
+    }
+
+    @Override
+    public void setMutationStrategy(MutationStrategy strategy) {
+        // Implementation for setting mutation strategy
     }
 }
